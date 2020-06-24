5077cf96603a600d12d634627986ff91aaf3a21a
Gary Chien <az20151114@gmail.com>
2020-06-24 10:01:44 +0800

    新增確認輸入文字的功能

--- index.js
+++ index.js
@@ -7,10 +7,6 @@ const bot = linebot({
     channelAccessToken: '5SIT7B39bQbnqLrJfvhTjW1ZMQr55ADLVjh+WhkfNLSr/asEKRUPN3xgu3b9ldBVAjstSAu+VtAGxJx6jeLo+2PpQPqCSjyn8RO6CTZv58+lo0AmKbNXPpJ/2zGBRGqa2gyiD0tmtPIU7ORDUuqraAdB04t89/1O/w1cDnyilFU='
 });
 
-bot.on('message', function (event) {
-    console.log(event); //把收到訊息的 event 印出來看看
-});
-
 const app = express();
 const linebotParser = bot.parser();
 app.post('/', linebotParser);
@@ -19,4 +15,33 @@ app.post('/', linebotParser);
 var server = app.listen(process.env.PORT || 8080, function () {
     var port = server.address().port;
     console.log("App now running on port", port);
+});
+
+
+bot.on('message', function (event) {
+    console.log(event); //把收到訊息的 event 印出來看看
+    if (event.type === 'message' && event.message.type === 'text') {
+        let userMessage = event.message.text;
+        event.reply({
+            type: 'template',
+            altText: 'this is a confirm template',
+            template: {
+                type: 'confirm',
+                text: `你輸入的文字是 (${userMessage}) ?`,
+                actions: [{
+                    type: 'postback',
+                    label: '對',
+                    data: '對'
+                }, {
+                    type: 'postback',
+                    label: '不對',
+                    data: '不對'
+                }]
+            }
+        })
+    }
+    else if (event.type === 'postback' && event.postback !== undefined) {
+        let userPostback = event.postback.data;
+        event.reply(`你的選擇是 (${userPostback})`);
+    }
 });
