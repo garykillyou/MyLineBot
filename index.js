bfa55bb9380b07506af4700f738661ab5e0855ff
Gary Chien <az20151114@gmail.com>
2020-06-24 09:16:32 +0800

    更改引用變數的前綴

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import linebot from 'linebot';
-import express from 'express';
+const linebot = require('linebot');
+const express = require('express');
 
-var bot = linebot({
+const bot = linebot({
     channelId: '1654379917',
     channelSecret: '6d2dfcba246e0e69c3db0921ba3d81db',
     channelAccessToken: '5SIT7B39bQbnqLrJfvhTjW1ZMQr55ADLVjh+WhkfNLSr/asEKRUPN3xgu3b9ldBVAjstSAu+VtAGxJx6jeLo+2PpQPqCSjyn8RO6CTZv58+lo0AmKbNXPpJ/2zGBRGqa2gyiD0tmtPIU7ORDUuqraAdB04t89/1O/w1cDnyilFU='
